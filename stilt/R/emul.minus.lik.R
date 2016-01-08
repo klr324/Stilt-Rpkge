6ecfe7cb4f986093656f5462c61d884bf5e82c56
Kelsey Ruckert <klr324@rsc64dot1x-37.ems.psu.edu>
2016-01-08 13:38:19 -0500

    Delete stilt folder
    Folder no longer needed with files and sub folders moved up a folder
    (Stilt-Rpkge folder).

--- stilt/R/emul.minus.lik.R
+++
@@ -1,8 +0,0 @@
-emul.minus.lik <-
-function(parvec, Y.mat, X.mat, t.vec, Theta.mat, n.par, p.par, fix.betas,
-                     limits.lower=NULL, limits.upper=NULL, beta.vec=NULL) { 
-  llik <- emul.lik(parvec, Y.mat, X.mat, t.vec,
-             Theta.mat, n.par, p.par, fix.betas, limits.lower, limits.upper, beta.vec)
-  mllik <- (-1)*llik 
-  mllik 
-}
